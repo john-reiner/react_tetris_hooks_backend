b0068933df20ebbb63df9d8910a39435430e1938
john-reiner <johnreiner1990@gmail.com>
2021-02-12 07:46:13 -0500

    removed user_params for testing

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -2,15 +2,23 @@ module Api
     module V2
         class UsersController < ApplicationController
             def create
-                user = User.create!(user_params)
-                render json: user
+                user = User.create!(
+                    username: params[:username],
+                    password: params[:password]
+                )
+
+                if user.save 
+                    render json: {status: 'SUCCESS', message: 'user Created', data:user},status: :ok
+                else 
+                    render json: {status: 'ERROR', message: 'user NOT Created', data:user.errors},status: :unprocessable_entity
+                end
             end
 
             private 
 
-            def user_params
-                params.require(:user).permit(:user_id, :username, :password)
-            end
+            # def user_params
+            #     params.require(:user).permit(:username, :password, :password_confirmation)
+            # end
         end
     end
 end
