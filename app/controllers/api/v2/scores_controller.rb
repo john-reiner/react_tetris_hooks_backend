c4673b4308ad9d604e2f7d8346d5202ab7a98ea8
john-reiner <johnreiner1990@gmail.com>
2021-03-29 11:10:38 -0400

    updated scores controller removed users

--- app/controllers/api/v2/scores_controller.rb
+++ app/controllers/api/v2/scores_controller.rb
@@ -1,16 +1,14 @@
 module Api
     module V2
         class ScoresController < ApplicationController
-            before_action :authenticate_user
             def index
                 scores = Score.all.limit(10)
-                user_scores = Score.where(user_id: @user.id)
-                render json: {all_scores: scores, user_scores: user_scores}, :include => {:user => {:only => [:username]}}
+                render json: {all_scores: scores}
             end
 
             def create
                 score = Score.create!(
-                    user_id: @user.id,
+                    name: parmas[:name],
                     points: params[:points],
                     rows: params[:rows],
                     level: params[:level]
